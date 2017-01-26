3503f95a4e5bd34bb147d26830b8db470b6d6fb4
Andrew Riley <andrew@andrewriley.info>
2017-01-26 23:13:13 +1100

    added logging

--- check.py
+++ check.py
@@ -1,12 +1,17 @@
-    
+import logging    
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 
 # GPIO 23 set up as input. It is pulled up to stop false signals  
 GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+#setup logging
+logging.basicConfig(filename='garage.log', level=logging.INFO)
+logging.info('Garage Door Check Started')
+
 # write door status changes
 def writeStatus():
+	#logging.info('writing current status')
         f = open ('garage.status','w')
         f.write(status)
         f.close
@@ -14,17 +19,21 @@ def writeStatus():
 
 #Check and write current status of door
 GPIO.input(23)
+print GPIO.input(23)
 
 if GPIO.input(23):
     print('Garage is currently Open')
     status = 'open'
     writeStatus()
+    logging.info('Garage is currently Open')
 else:
     print('Garage is currently Closed')
     status = 'closed'
     writeStatus()
+    logging.info('Garage is currently Closed')
 
 print 'Waiting for status change'
+
 #run interrupt 'while' to track door status changes
 var = 1 
 while var == 1 :
@@ -32,11 +41,13 @@ while var == 1 :
 	print "\nDoor Open"
 	status = 'open'
 	writeStatus()
+	logging.info('Garage is now Open')
 
 	GPIO.wait_for_edge(23, GPIO.FALLING)
 	print "\nDoor Closed"
 	status = 'closed'
 	writeStatus()
+	logging.info('Garage is now Closed')
 
 GPIO.cleanup()           # clean up GPIO on normal exit  
 
